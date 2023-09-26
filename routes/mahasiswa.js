b3a67b4f80fed105373c48bc411c607abaea6f88
nafiafatimah <sitinafiatulfatimah@gmail.com>
2023-09-26 10:10:40 +0700

    modul-4

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -53,6 +53,31 @@ router.post('/store',[
 })
 
 
+router.get('/(:id)', function (req, res) {
+    let id = req.params.id;
+    connection.query(`select * from mahasiswa where id_m = ${id}`, function(err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        }
+        if(rows.lenght <=0){
+            return res.status(404).json({
+                status: false,
+                message: 'Not Found',
+            })
+        }
+        else{
+            return res.status(200).json({
+                status: true,
+                message: 'Data Mahasiswa',
+                data: rows[0]
+            })
+        }
+    })
+})
+
 
 
 module.exports = router;
