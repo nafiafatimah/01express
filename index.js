c6781639317f1b3c455cac4ae32cd1b924ba23b8
nafiafatimah <sitinafiatulfatimah@gmail.com>
2023-09-26 09:58:00 +0700

    modul-3

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require ('express')
 const app = express()
 const port = 3000
 
+//membuat route baru dengan method GET yang isinya hallo...
+const bodyPs = require('body-parser'); //import body-paarser
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
 const  mhsRouter = require('./routes/mahasiswa');
 app.use('/api/mhs', mhsRouter);
 
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const router = express.Router();
-
 const connection = require('../config/db.js');
+const {body, validationResult } = require('express-validator');
+
 
 router.get('/', function (req, res){
     connection.query('select * from mahasiswa order by id_m desc', function(err, rows){
@@ -19,4 +20,39 @@ router.get('/', function (req, res){
         }
     })
 });
+
+router.post('/store',[
+    //validation
+    body('nama').notEmpty(),
+    body('nrp').notEmpty()
+],(req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()
+        });
+    }
+    let Data = {
+        nama: req.body.nama,
+        nrp: req.body.nrp
+    }
+    connection.query('insert into mahasiswa set ?', Data, function(err, rows){
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        }else{
+            return res.status(201).json({
+                status: true,
+                message: 'Success..!',
+                data: rows[0]
+            })
+        }
+    })
+})
+
+
+
+
 module.exports = router;
